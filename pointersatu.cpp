5eeae24990ec6b343fcf2892ef323d24a8dca47e
Hussain512 <shujaatfida512@gmail.com>
2026-05-05 12:06:44 +0700

    Add reference usage example

--- pointersatu.cpp
+++ pointersatu.cpp
@@ -16,3 +16,12 @@ cout<<"No Induk ="<<nim<<endl;
 }
 
 };
+int main(){
+
+mahasiswa mhs{1}; // Object mhs
+
+mhs.showNim(); //Member Access Operator
+
+mahasiswa &refMhs = mhs; //Pointer Reference refMhs
+
+refMhs.nim = 2; //Member Access Operator
