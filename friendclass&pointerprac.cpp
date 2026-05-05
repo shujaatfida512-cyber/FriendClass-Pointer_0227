0d1ad9e6574bee731fedc751477bc5d0dc66bc78
Hussain512 <shujaatfida512@gmail.com>
2026-05-05 12:36:05 +0700

    Add Kite class with inputData method

--- friendclass&pointerprac.cpp
+++ friendclass&pointerprac.cpp
@@ -1,2 +1,19 @@
 #include <iostream>
 using namespace std;
+
+class Rhombus;
+
+class Kite 
+{
+private:
+    float diagonal1, diagonal2;
+    float sideA, sideB;
+
+public:
+    void inputData() {
+        cout << "Enter Kite Data\n";
+        cout << "Diagonal 1: "; cin >> diagonal1;
+        cout << "Diagonal 2: "; cin >> diagonal2;
+        cout << "Side A: "; cin >> sideA;
+        cout << "Side B: "; cin >> sideB;
+    }
