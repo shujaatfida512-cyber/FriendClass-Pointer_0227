54b876ac16c66c44f06fc653869e58aa561d879f
Hussain512 <shujaatfida512@gmail.com>
2026-05-05 12:13:48 +0700

    Implement showNilaiPelajar and test in main

--- friendFunctiondua.cpp
+++ friendFunctiondua.cpp
@@ -16,3 +16,15 @@ public:
     pelajar() { nilai = 100; }
     friend void manusia::showNilaiPelajar(pelajar &x);
 };
+void manusia::showNilaiPelajar(pelajar &x)
+{
+    cout << x.nilai;
+}
+
+int main()
+{
+    manusia budi;
+    pelajar pbudi;
+    budi.showNilaiPelajar(pbudi);
+    return 0;
+}
